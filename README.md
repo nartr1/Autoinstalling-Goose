5b72dcef3962f53f747f106b38637818d90de066
failure-to-resolve <transaction.failed.0@gmail.com>
2020-03-11 13:50:09 -0600

    Hype is real

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Autoinstalling-Goose
+This is the most beautiful program to ever have graced your motherboard.
--- goose_installer.sh
+++ goose_installer.sh
@@ -2,6 +2,6 @@
 
 #apt install wine winetricks -y
 
-cat goose64 | base64 -d >> goose_zip && rm -rf goose64 && unzip goose_zip && wine DesktopGoose\ v0.3/GooseDesktop.exe
-
-rm -rf DesktopGoose\ v0.3/
+cat goose64 | base64 -d >> goose_zip && unzip goose_zip && rm -rf goose_zip
+wine DesktopGoose\ v0.3/GooseDesktop.exe
+#rm -rf DesktopGoose\ v0.3/
